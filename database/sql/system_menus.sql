d42daa4cf26ddce4cf967efab34f3a00b079bf28
TieHanHan <1450603827@qq.com>
2024-10-25 17:59:24 +0800

    1. 更新测试数据

--- database/sql/system_menus.sql
+++ database/sql/system_menus.sql
@@ -18,13 +18,13 @@ INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (15, 12, '修改', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.admin.update', '2', NULL, '2024-08-21 07:30:30', '2024-08-21 07:30:30');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (16, 3, '列表', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.role.index', '2', NULL, '2024-08-21 07:30:30', '2024-08-21 07:30:30');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (17, 7, '列表', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.menu.index', '2', NULL, '2024-08-21 07:30:30', '2024-08-21 07:30:30');
-INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (18, 12, '列表', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.amidn.index', '2', NULL, '2024-08-21 07:30:30', '2024-08-21 07:30:30');
+INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (18, 12, '列表', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.admin.index', '2', NULL, '2024-08-21 07:30:30', '2024-08-21 07:30:30');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (19, 11, '主体管理', NULL, NULL, 'N', 'setting_system_company', '/setting/system/company', 'view.setting_system_company', 'raphael:user', '[]', '', '1', NULL, '2024-08-21 07:28:16', '2024-08-21 07:28:16');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (20, 19, '添加', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.company.store', '2', NULL, '2024-08-21 07:29:06', '2024-08-21 07:29:06');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (21, 19, '删除', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.company.delete', '2', NULL, '2024-08-21 07:31:37', '2024-08-21 07:31:37');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (22, 19, '修改', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.company.update', '2', NULL, '2024-08-21 07:30:30', '2024-08-21 07:30:30');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (23, 19, '列表', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.company.index', '2', NULL, '2024-08-21 07:30:30', '2024-08-21 07:30:30');
-INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (24, 0, '设置', NULL, NULL, 'N', 'setting', '/setting', 'view.setting', 'ant-design:setting-filled', '[]', '', '1', NULL, NULL, NULL);
+INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (24, 0, '设置', NULL, NULL, 'N', 'setting', '/setting', 'view.setting', 'ant-design:setting-filled', '[]', '', '1', NULL, '2024-08-21 07:30:30', '2024-08-21 07:30:30');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (25, 0, '财务中心', NULL, 'route.finance', 'N', 'finance', '/finance', 'view.finance', 'carbon:finance', '[]', NULL, '1', NULL, '2024-10-24 11:13:07', '2024-10-24 11:13:07');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (26, 25, '基础资料', NULL, 'route.finance_basic-data', 'N', 'finance_basic-data', '/finance/basic-data', 'view.finance_basic-data', 'fluent:data-usage-edit-20-filled', '[]', NULL, '1', NULL, '2024-10-24 11:19:00', '2024-10-24 11:19:00');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (27, 26, '会计科目', NULL, 'route.finance_basic-data_subject', 'N', 'finance_basic-data_subject', '/finance/basic-data/subject', 'view.finance_basic-data_subject', 'carbon:account', '[]', NULL, '1', NULL, '2024-10-24 11:30:38', '2024-10-24 11:45:22');
@@ -39,6 +39,11 @@ INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (36, 37, '付款单', NULL, 'route.warehouse_payment-order', 'N', 'warehouse_payment-order', '/warehouse/payment-order', 'view.warehouse_payment-order', 'fluent:payment-16-regular', '[]', NULL, '1', NULL, '2024-10-24 16:36:22', '2024-10-24 16:39:33');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (37, 28, '资金', NULL, 'route.warehouse_funds', 'N', 'warehouse_funds', '/warehouse/funds', 'view.warehouse_funds', 'icon-park-outline:funds', '[]', NULL, '1', NULL, '2024-10-24 16:39:22', '2024-10-24 16:39:22');
 INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (38, 37, '收款单', NULL, 'route.warehouse_funds_receiving-order', 'N', 'warehouse_funds_receiving-order', '/warehouse/funds/receiving-order', 'view.warehouse_funds_receiving-order', 'ant-design:money-collect-outlined', '[1, 2]', NULL, '1', NULL, '2024-10-24 16:41:03', '2024-10-24 16:43:36');
+INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (39, 3, '分配权限', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.role.assign-permission', '2', NULL, '2024-10-25 17:27:26', '2024-10-25 17:27:26');
+INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (40, 3, '获取所有角色', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.role.get-roles', '2', NULL, '2024-10-25 17:31:22', '2024-10-25 17:31:22');
+INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (41, 7, '获取所有菜单和权限', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.role.get-menus-and-permissions', '2', NULL, '2024-10-25 17:32:24', '2024-10-25 17:32:24');
+INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (42, 7, '获取所有菜单', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '[]', 'system.role.get-all-menus', '2', NULL, '2024-10-25 17:34:42', '2024-10-25 17:34:42');
+INSERT INTO `ops`.`system_menus` (`id`, `pid`, `title`, `sort`, `i18n_key`, `is_hide_menu`, `name`, `path`, `component`, `icon`, `com_id`, `permission`, `type`, `deleted_at`, `created_at`, `updated_at`) VALUES (43, 19, '获取所有主体', NULL, 'route.setting_system_company', NULL, NULL, NULL, NULL, NULL, '[]', 'system.role.get-all-companies', '2', NULL, '2024-10-25 17:38:40', '2024-10-25 17:38:40');
 
 -- ----------------------------
 -- Records of system_admin_roles
@@ -51,6 +56,9 @@ INSERT INTO `ops`.`system_admin_roles` (`admin_id`, `role_id`) VALUES (4, 2);
 INSERT INTO `ops`.`system_admin_roles` (`admin_id`, `role_id`) VALUES (5, 3);
 INSERT INTO `ops`.`system_admin_roles` (`admin_id`, `role_id`) VALUES (6, 4);
 INSERT INTO `ops`.`system_admin_roles` (`admin_id`, `role_id`) VALUES (7, 2);
+INSERT INTO `ops`.`system_admin_roles` (`admin_id`, `role_id`) VALUES (9, 3);
+INSERT INTO `ops`.`system_admin_roles` (`admin_id`, `role_id`) VALUES (9, 4);
+INSERT INTO `ops`.`system_admin_roles` (`admin_id`, `role_id`) VALUES (13, 5);
 
 -- ----------------------------
 -- Records of system_role_menus
@@ -79,6 +87,73 @@ INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (2, 21);
 INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (2, 22);
 INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (2, 23);
 INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (2, 24);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 1);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 2);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 24);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 11);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 3);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 4);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 5);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 6);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 16);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 7);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 8);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 9);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 10);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 17);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 12);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 13);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 14);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 15);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 18);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 19);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 20);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 21);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 22);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 23);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 25);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 26);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 27);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 28);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 30);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 31);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 32);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 37);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 36);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 38);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 29);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 33);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 34);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (1, 35);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (3, 2);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (3, 1);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (4, 2);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (4, 1);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 24);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 11);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 3);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 7);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 8);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 9);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 10);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 17);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 12);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 19);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 1);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 2);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (3, 25);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (3, 26);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (3, 27);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (4, 29);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (5, 41);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (4, 33);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (4, 34);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (4, 35);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (4, 4);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (4, 5);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (4, 6);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (4, 16);
+INSERT INTO `ops`.`system_role_menus` (`role_id`, `menu_id`) VALUES (4, 39);
 
 
 -- ----------------------------
@@ -100,6 +175,9 @@ INSERT INTO `ops`.`system_admin_companies` (`admin_id`, `company_id`) VALUES (4,
 INSERT INTO `ops`.`system_admin_companies` (`admin_id`, `company_id`) VALUES (5, 1);
 INSERT INTO `ops`.`system_admin_companies` (`admin_id`, `company_id`) VALUES (6, 2);
 INSERT INTO `ops`.`system_admin_companies` (`admin_id`, `company_id`) VALUES (7, 3);
+INSERT INTO `ops`.`system_admin_companies` (`admin_id`, `company_id`) VALUES (9, 1);
+INSERT INTO `ops`.`system_admin_companies` (`admin_id`, `company_id`) VALUES (9, 2);
+
 
 
 
